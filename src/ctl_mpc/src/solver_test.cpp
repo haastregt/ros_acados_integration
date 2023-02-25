4a27b38f7c37d3030c7d0b422577a48b19c957f0
Jonne van Haastregt <jonnevanhaastregt@gmail.com>
2023-02-25 19:04:49 +0100

    Initial commit

---
+++ src/ctl_mpc/src/solver_test.cpp
@@ -0,0 +1,9 @@
+#include "acados_solver_astrobee.h"
+
+int main()
+{
+    // capsule = astrobee_acados_create_capsule()
+    // solve_status = astrobee_acados_solve(capsule)
+    // return solve_status
+    return 0;
+}
